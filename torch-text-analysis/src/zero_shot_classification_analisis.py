59d0c4e30fba205b2ca57c8dca4f98f48226bf45
Giovanni Foletto <giovanni.foletto@gmail.com>
2024-06-12 10:01:01 +0000

    Added latest notebook

--- torch-text-analysis/src/zero_shot_classification_analisis.py
+++ torch-text-analysis/src/zero_shot_classification_analisis.py
@@ -6,16 +6,15 @@ classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnl
 
 log_description = ["malicious", "benign"]
 openfile = open("../../data/raw/unificated.ndjson")
-lines = openfile.readlines()[83:15000]
+lines = openfile.readlines()[:15000]
 
 #outputfile = open("./results_transformers_line_by_line.res", "w")
 outputfile = open("./results_transformers2.res", "a")
 
 log_description = ["malicious", "benign"]
-log_description2 = ["anomalous", "normal"]
 for line in tqdm(lines):
     #results = classifier(line, log_description)
-    results2 = classifier(line, log_description2)
+    results2 = classifier(line, log_description)
 
     # for label, score in zip(results["labels"], results["scores"]):
     #     outputfile.write("\n")
